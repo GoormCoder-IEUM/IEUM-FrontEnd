90689867e7ffdfe667c495d221904dcca34cd876
Kimsumin123 <tntn211@naver.com>
2024-07-19 12:28:41 +0900

    이동수단 선택 모달

--- src/modal/CarChoose.js
+++ src/modal/CarChoose.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const CarChoose = () => {
-  return <h2>CarChoose Page</h2>;
+  return (
+    <div>
+      <h2>이동 수단</h2>
+      <p>여행 시 이용하실 이동수단 선택해주세요.</p>
+      <h4>택시나 자가용, 렌트카 등을 이용</h4>
+      <button>대중교통</button>
+      <button>승용차</button>
+      <button>닫기</button>
+    </div>
+  );
 };
 
 export default CarChoose;
