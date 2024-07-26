5b091e4734a89bd433574c207b172562455dbecf
mingi choo <cn78943@gmail.com>
2024-07-26 16:27:30 +0900

    일정 기능 추가, 파일명 수정

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import "../style/Schedule.css";
-import Step1 from "../sidebar/Step1";
-import Step2 from "../sidebar/Step2";
-import Step3 from "../sidebar/Step3";
+import SetDate from "../sidebar/SetDate";
+import SetPlace from "../sidebar/SetPlace";
+import SetAccommodation from "../sidebar/SetAccommodation";
 import DateChooseModal from "../modal/DateChooseModal";
+import InviteMemberModal from "../modal/InviteMemberModal";
 
 const Schedule = () => {
   const [activeStep, setActiveStep] = useState("STEP 1");
-  const [showModal, setShowModal] = useState(true);
+  const [showDateChooseModal, setShowDateChooseModal] = useState(true);
+  const [showInviteMemberModal, setShowInviteMemberModal] = useState(false);
   const [selectedDates, setSelectedDates] = useState("");
+  const [planId, setPlanId] = useState(null); // Add planId state
   const location = useLocation();
   const { id, krName } = location.state || {}; 
 
@@ -21,18 +24,23 @@ const Schedule = () => {
     setActiveStep(step);
   };
 
-  const closeModal = () => {
-    setShowModal(false);
+  const closeDateChooseModal = () => {
+    setShowDateChooseModal(false);
+    setShowInviteMemberModal(true);
+  };
+
+  const closeInviteMemberModal = () => {
+    setShowInviteMemberModal(false);
   };
 
   const renderComponent = (step) => {
     switch (step) {
       case "STEP 1":
-        return <Step1 selectedDates={selectedDates} krName={krName} />;
+        return <SetDate selectedDates={selectedDates} krName={krName} />;
       case "STEP 2":
-        return <Step2 selectedDates={selectedDates} krName={krName} id={id} />;
+        return <SetPlace selectedDates={selectedDates} krName={krName} id={id} />;
       case "STEP 3":
-        return <Step3 selectedDates={selectedDates} />;
+        return <SetAccommodation selectedDates={selectedDates} />;
       default:
         return null;
     }
@@ -40,7 +48,18 @@ const Schedule = () => {
 
   return (
     <div className="container">
-      <DateChooseModal show={showModal} onClose={closeModal} setSelectedDates={setSelectedDates} id={id} />
+      <DateChooseModal 
+        show={showDateChooseModal} 
+        onClose={closeDateChooseModal} 
+        setSelectedDates={setSelectedDates} 
+        id={id} 
+        setPlanId={setPlanId} // Pass setPlanId to DateChooseModal
+      />
+      <InviteMemberModal 
+        show={showInviteMemberModal} 
+        onClose={closeInviteMemberModal} 
+        planId={planId} // Pass planId to InviteMemberModal
+      />
       <div className="sidebar-container">
         <div className="sidebar">
           <div className="sidebar-detail" onClick={() => handleSetActiveStep("STEP 1")}>
@@ -49,11 +68,11 @@ const Schedule = () => {
           </div>
           <div className="sidebar-detail" onClick={() => handleSetActiveStep("STEP 2")}>
             <div>STEP 2</div>
-            <div>날짜 확인</div>
+            <div>장소 선택</div>
           </div>
           <div className="sidebar-detail" onClick={() => handleSetActiveStep("STEP 3")}>
             <div>STEP 3</div>
-            <div>날짜 확인</div>
+            <div>숙소 선택</div>
           </div>
         </div>
       </div>
