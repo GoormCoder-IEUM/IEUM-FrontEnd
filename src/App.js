c03e44f7382cee9c46ca3e06e557ef7ff6592c31
mingi choo <cn78943@gmail.com>
2024-07-30 12:49:39 +0900

    불필요한 부분 삭제

--- src/App.js
+++ src/App.js
@@ -11,11 +11,6 @@ import HeaderBar from "./pages/HeaderBar";
 import SelectDestination from "./pages/SelectDestination";
 import SelectAccommodation from "./pages/SelectAccommodation";
 import FinalData from "./pages/FinalData";
-import ContryIntroModal from "./modal/ContryIntroModal";
-import DateChooseModal from "./modal/DateChooseModal";
-import PlaceIntroModal from "./modal/PlaceIntroModal";
-import StayIntroModal from "./modal/StayIntroModal";
-import KakaoMap from "./pages/KakaoMap";
 
 
 function App() {
@@ -32,7 +27,6 @@ function App() {
         <Route path="/select-accommodation" element={<SelectAccommodation />} />
         <Route path="/finaldata" element={<FinalData />} />
       </Routes>
-      <KakaoMap/>
     </Router>
   );
 }
