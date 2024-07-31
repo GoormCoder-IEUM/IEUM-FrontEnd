3618d9bc829673e9a966d596d0e8e9ef74fdc1eb
mingi choo <cn78943@gmail.com>
2024-07-31 18:43:33 +0900

    사용 하지 않는 import 삭제

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../style/Login.css";
 
