836bc6f2f283eb3c7086e4a05329b6a4be431e14
mingi choo <cn78943@gmail.com>
2024-07-31 18:44:42 +0900

    특정 일정에 대해 수정기능 추가

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -6,6 +6,8 @@ import SetPlace from "../sidebar/SetPlace";
 import SetAccommodation from "../sidebar/SetAccommodation";
 import DateChooseModal from "../modal/DateChooseModal";
 import InviteMemberModal from "../modal/InviteMemberModal";
+import KakaoMap from "./KakaoMap";
+import axios from "axios";
 
 const Schedule = () => {
   const [activeStep, setActiveStep] = useState("STEP 1");
@@ -14,11 +16,26 @@ const Schedule = () => {
   const [selectedDates, setSelectedDates] = useState("");
   const [planId, setPlanId] = useState(null);
   const location = useLocation();
-  const { destinationId, krName } = location.state || {}; 
+  const { destinationId, krName: initialKrName, planIdForState } = location.state || {};
+  const [krName, setKrName] = useState(initialKrName);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
 
   useEffect(() => {
-    console.log("Location State:", location.state); // Add this line
-  }, [location.state]);
+    if (krName === undefined) {
+      setShowDateChooseModal(false);
+      console.log("planIdForState ", planIdForState);
+      setPlanId(planIdForState);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (planId !== null) {
+      console.log("set plan id : ", planId);
+      fetchPlanData();
+    }
+  }, [planId]);
 
   const handleSetActiveStep = (step) => {
     setActiveStep(step);
@@ -46,6 +63,41 @@ const Schedule = () => {
     }
   };
 
+  const formatDate = (date) => {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const day = String(d.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  };
+
+const fetchPlanData = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(`http://localhost:8080/plans/${planId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const startDate = new Date(response.data.startedAt).toISOString().split('T')[0];
+    const endDate = new Date(response.data.endedAt).toISOString().split('T')[0];
+    
+    setStartDate(startDate);
+    setEndDate(endDate);
+    
+    const selectedDates = `${startDate} ~ ${endDate}`;
+    setSelectedDates(selectedDates);
+    console.log("selectedDates ", selectedDates);
+
+    setKrName(response.data.destinationKrName);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+
   return (
     <div className="container">
       <DateChooseModal 
@@ -54,6 +106,7 @@ const Schedule = () => {
         setSelectedDates={setSelectedDates} 
         destinationId={destinationId} 
         setPlanId={setPlanId} // Pass setPlanId to DateChooseModal
+        formatDate={formatDate}
       />
       <InviteMemberModal 
         show={showInviteMemberModal} 
@@ -79,6 +132,7 @@ const Schedule = () => {
       <div className="content-container">
         {renderComponent(activeStep)}
       </div>
+      <KakaoMap />
     </div>
   );
 };
