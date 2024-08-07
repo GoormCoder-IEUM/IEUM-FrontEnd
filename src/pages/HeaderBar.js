a4bdd84f80f72ae1c2d4ee48ea3b03c4527dee85
Kimsumin123 <tntn211@naver.com>
2024-08-07 11:05:26 +0900

    로그 아웃 기능 구현

--- src/pages/HeaderBar.js
+++ src/pages/HeaderBar.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../style/HeaderBar.css";
 
 const HeaderBar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // 로컬 스토리지에서 토큰 제거
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+
+    // 로그인 페이지로 리다이렉트
+    navigate("/login");
+  };
+
   return (
     <nav>
       <ul>
@@ -31,7 +42,10 @@ const HeaderBar = () => {
           <Link to="/finaldata">Finaldata</Link>
         </li>
         <li>
-          <Link to = "/chat">chat</Link>
+          <Link to="/chat">Chat</Link>
+        </li>
+        <li>
+          <button onClick={handleLogout}>Logout</button>
         </li>
       </ul>
     </nav>
